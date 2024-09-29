500c9ff74da4a77f2c6814181dfd289aa67aad36
João Nascimento <joaombn97@gmail.com>
2024-09-29 00:43:50 -0300

    013

---
+++ 013-candy.go
@@ -0,0 +1,44 @@
+package main
+
+func candy(ratings []int) int {
+	size := len(ratings)
+
+	forwardStreak := make([]int, size)
+	forwardStreak[0] = 1
+	for i := 1; i < size; i++ {
+		if ratings[i] > ratings[i-1] {
+			forwardStreak[i] = forwardStreak[i-1] + 1
+		} else {
+			forwardStreak[i] = 1
+		}
+	}
+
+	backwardStreak := make([]int, size)
+	backwardStreak[size-1] = 1
+	for i := size - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			backwardStreak[i] = backwardStreak[i+1] + 1
+		} else {
+			backwardStreak[i] = 1
+		}
+	}
+
+	totalCandies := 0
+
+	for i := 0; i < size; i++ {
+		rating := ratings[i]
+		candies := 1
+
+		if i > 0 && rating > ratings[i-1] {
+			candies = forwardStreak[i-1] + 1
+		}
+
+		if i < size-1 && rating > ratings[i+1] {
+			candies = max(candies, backwardStreak[i+1]+1)
+		}
+
+		totalCandies += candies
+	}
+
+	return totalCandies
+}
