57b583848c52f8867ab5fac3dbd4a9b111d8a9ca
João Nascimento <joaombn97@gmail.com>
2025-08-21 18:27:32 -0300

    125

---
+++ 125-validPalindrome.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+
+	re := regexp.MustCompile(`[^\d\w]|_`)
+	s = string(re.ReplaceAll([]byte(s), []byte("")))
+
+	s = strings.ToLower(s)
+	size := len(s)
+
+	if size == 0 {
+		return true
+	}
+
+	halfway := ((size + 1) / 2)
+	fmt.Printf("%v \n", halfway)
+
+	for i := range halfway {
+		forward := s[i]
+		backward := s[size-i-1]
+
+		if forward != backward {
+			return false
+		}
+	}
+
+	return true
+}
