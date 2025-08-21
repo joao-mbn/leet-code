d93fb542dc3449f805ffe6987afcddaa633b9f2d
João Nascimento <joaombn97@gmail.com>
2025-08-21 18:27:36 -0300

    209

---
+++ 209-minSubArray.go
@@ -0,0 +1,31 @@
+package main
+
+func minSubArrayLen(target int, nums []int) int {
+	minLength := 0
+	track := 0
+	sum := 0
+
+	for i, curr := range nums {
+		if curr >= target {
+			return 1
+		}
+
+		sum += curr
+		for sum >= target {
+			if minLength == 0 {
+				minLength = i - track + 1
+			} else {
+				minLength = min(i-track+1, minLength)
+			}
+
+			if minLength == 1 {
+				return 1
+			}
+
+			sum -= nums[track]
+			track++
+		}
+	}
+
+	return minLength
+}
