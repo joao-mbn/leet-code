9222a6ad23bb7c0f3cde3e0e3184205e764dc70e
João Nascimento <joaombn97@gmail.com>
2025-08-29 10:15:46 -0300

    105

---
+++ 105-binaryTreeFromInorderAndPreorder.go
@@ -0,0 +1,74 @@
+package main
+
+import "slices"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+
+	if len(preorder) == 1 {
+		return root
+	}
+
+	rootIndex := slices.Index(inorder, preorder[0])
+	if len(preorder[1:rootIndex+1]) > 0 {
+		root.Left = buildTree(preorder[1:rootIndex+1], inorder[:rootIndex])
+	}
+
+	if len(preorder[rootIndex+1:]) > 0 {
+		root.Right = buildTree(preorder[rootIndex+1:], inorder[rootIndex+1:])
+	}
+
+	return root
+}
+
+/*
+1. Find index of inorder equal to first element of preorder
+2. For inorder: Get slice of inorder to the left of index as left, the other as right
+3. For preorder: Get N elements past the first, where N = index - 1 as left, the other as right
+4. Are there two elements or less ? Build and return the tree : return call to step 1 with slices
+
+D = 0
+
+Inorder
+4, 2, 9, 5, 1, 6, 3, 15, 7
+
+Preorder
+1, 2, 4, 5, 9, 3, 6, 7, 15
+
+--------------------------------
+D = 1
+
+Inorder
+4, 2, 9, 5
+
+Preorder
+2, 4, 5, 9
+
+--------------------------------
+D = 2
+
+Inorder
+4, 2
+
+Preorder
+2, 4
+
+--------------------------------
+
+*/
--- main.go
+++ main.go
@@ -5,23 +5,27 @@ import "fmt"
 
 func main() {
 	for _, input := range []struct {
-		l1 *ListNode
-		l2 *ListNode
+		preorder []int
+		ineorder []int
 	}{
 		{
-			l1: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}},
-			l2: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}},
+			preorder: []int{1, 2},
+			ineorder: []int{1, 2},
 		},
 		{
-			l1: &ListNode{Val: 0},
-			l2: &ListNode{Val: 0},
+			preorder: []int{1, 2, 4, 5, 9, 3, 6, 7, 15},
+			ineorder: []int{4, 2, 9, 5, 1, 6, 3, 15, 7},
 		},
 		{
-			l1: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9}}},
-			l2: &ListNode{Val: 9, Next: &ListNode{Val: 9}},
+			preorder: []int{3, 9, 20, 15, 7},
+			ineorder: []int{9, 3, 15, 20, 7},
+		},
+		{
+			preorder: []int{-1},
+			ineorder: []int{-1},
 		},
 	} {
-		result := addTwoNumbers(input.l1, input.l2)
-		fmt.Printf("Given the inputs: %v and %v, the result is: %v\n", input.l1, input.l2, result)
+		result := buildTree(input.preorder, input.ineorder)
+		fmt.Printf("Given the inputs: %v and %v, the result is: %v\n", input.preorder, input.ineorder, result)
 	}
 }
