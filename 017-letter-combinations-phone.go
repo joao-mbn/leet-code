fea67e0c354f8cab0c6f0d0e6b6d1d0ba1670a24
João Nascimento <joaombn97@gmail.com>
2025-09-12 16:19:31 -0300

    017

---
+++ 017-letter-combinations-phone.go
@@ -0,0 +1,34 @@
+package main
+
+var letters = map[string][]string{
+	"2": {"a", "b", "c"},
+	"3": {"d", "e", "f"},
+	"4": {"g", "h", "i"},
+	"5": {"j", "k", "l"},
+	"6": {"m", "n", "o"},
+	"7": {"p", "q", "r", "s"},
+	"8": {"t", "u", "v"},
+	"9": {"w", "x", "y", "z"},
+}
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+
+	lettersOfFirstDigit := letters[string(digits[0])]
+	if len(digits) == 1 {
+		return lettersOfFirstDigit
+	}
+
+	combinations := []string{}
+	innerResults := letterCombinations(digits[1:])
+
+	for _, result := range innerResults {
+		for _, letter := range lettersOfFirstDigit {
+			combinations = append(combinations, letter+result)
+		}
+	}
+
+	return combinations
+}
