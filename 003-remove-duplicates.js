9b5df3b3c0ada69d1cb996374eccb312baed92a9
João Nascimento <joaombn97@gmail.com>
2025-09-17 20:08:36 -0300

    uniting v2 files under the same file

--- 003-remove-duplicates.js
+++ 003-remove-duplicates.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
+  let uniqueCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[uniqueCount] = nums[i];
+      uniqueCount++;
+    }
+  }
+
+  return uniqueCount;
+};
+
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+removeDuplicates(nums);
+console.log(nums);
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+var removeDuplicates = function (nums) {
   let uniqueCount = nums.length;
   let lastUnique;
 
@@ -31,3 +51,4 @@ var removeDuplicates = function (nums) {
 const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 removeDuplicates(nums);
 console.log(nums);
+ */
--- 009-jump-game.js
+++ 009-jump-game.js
@@ -3,6 +3,32 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
+  if (nums.length === 1) return true;
+  if (nums[0] === 0) return false;
+
+  let maxJump = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (i > maxJump) {
+      return false; // exceeded the jump capacity without reaching the last index without any chance to update to a greater position
+    }
+
+    if (i + nums[i] > maxJump) {
+      maxJump = i + nums[i]; // encompasses all jump possibilities of the previous index plus more
+    }
+
+    if (maxJump >= nums.length - 1) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+var canJump = function (nums) {
   const zeroesIndexes = [];
 
   for (let i = 0; i < nums.length; i++) {
@@ -42,3 +68,4 @@ var canJump = function (nums) {
 
   return canJumpFromPosition(0);
 };
+*/
