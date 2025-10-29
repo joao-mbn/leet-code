4f31034f1a1a3e2be1463596a05ed7bfcab13ddf
João Nascimento <joaombn97@gmail.com>
2025-10-29 13:48:48 -0700

    maximum-subarray

--- array-string/maximum-subarray.js
+++ array-string/maximum-subarray.js
@@ -25,10 +25,3 @@ var maxSubArray = function(nums) {
   return maxSum
 };
 
-console.log(maxSubArray([-2,3,1,3]))
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
-console.log(maxSubArray([1]))
-console.log(maxSubArray([5,4,-1,7,8]))
-console.log(maxSubArray([-1,-2]))
-console.log(maxSubArray([5000,1,9999,1999]))
-console.log(maxSubArray([1,4,-9999,5,6]))
