cbc4c5b0aaed1a6802323db8e36be71e5a8e3760
João Nascimento <joaombn97@gmail.com>
2025-10-31 11:56:39 -0700

    lowest-common-ancestor-of-a-binary-search-tree

---
+++ binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  while (true) {
+    if (
+      (root.val === p.val || root.val === q.val) ||
+      (root.val > p.val && root.val < q.val) ||
+      (root.val < p.val && root.val > q.val)
+    ) {
+      return root
+    }
+
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left
+    }
+  }
+};
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
