56fd045b98d1ac1a287bd76bb9f428d00571dca0
João Nascimento <joaombn97@gmail.com>
2025-11-05 14:41:39 -0800

    merge-sort

---
+++ sort/merge-sort.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var mergeSort = function(nums) {
+  if (nums.length <= 1) {
+    return nums
+  }
+
+  const halfway = Math.floor(nums.length/2)
+  const sortedLeft = mergeSort(nums.slice(0, halfway))
+  const sortedRight = mergeSort(nums.slice(halfway))
+  const sortedNums = []
+  let i = j = 0
+
+  while (i < sortedLeft.length && j < sortedRight.length) {
+    if (sortedLeft[i] < sortedRight[j]) {
+      sortedNums.push(sortedLeft[i])
+      i++
+    } else {
+      sortedNums.push(sortedRight[j])
+      j++
+    }
+  }
+
+  sortedNums.push(...sortedLeft.slice(i), ...sortedRight.slice(j))
+
+  return sortedNums
+}
+
+console.log(mergeSort([3, 2, 5, 0, 1, 8, 7, 6, 9, 4]))
